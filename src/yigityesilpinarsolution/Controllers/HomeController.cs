920801f88f7cc737f5704e060854cf1629c1e187
Yigit Yesilpinar <yigityesilpinar@gmail.com>
2016-05-20 21:49:08 +0200

    MVC core setup

---
+++ src/yigityesilpinarsolution/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace yigityesilpinarsolution.Controllers
+{
+    // Derived from Mvc Controller Base class
+    public class HomeController : Controller
+    {
+
+
+        [AllowAnonymous]
+        public ViewResult Index()
+        {
+            //var model = new HomePageViewModel();
+            return View();
+        }
+     
+    }
+}
